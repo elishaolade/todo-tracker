ff22b0f136ac3c9a00ca54fbd3e60522cb9955d9
elishaolade <elishaolade@gmail.com>
2021-09-24 21:50:47 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/todoItem';
+import './components/todoList';
+import TodoForm from './components/todoForm';
+import TodoList from './components/todoList';
+import React, {useState, useEffect} from 'react';
+import generateRandomString from './utils/js/randomString';
 
 function App() {
+
+  const [todo, setTodo] = useState([]);
+  const [complete, setComplete] = useState([]);
+  const [done, setDone] = useState(false);
+
+  useEffect(()=> {
+    
+  })
+
+  const addItem = (text) => {
+    const randomNum = generateRandomString();
+    const item = { id: randomNum, text: text, done: false };
+    setTodo(todo => [...todo, item]);
+    console.log('todo:', todo)
+  }
+
+  const addComplete = (id) => {
+    const item = findItem(id);
+    const newItem = {
+      id: item.id,
+      text: item.text,
+      done: true
+    }
+    const filteredtoDos = todo.filter(item => item.id !== id);
+    setComplete([...complete, newItem]);
+    setTodo(filteredtoDos);
+  }
+
+  const findItem = (id) => {
+    return todo.find(item => item.id === id);
+  }
+
+  const findComplete = (id) => {
+    return complete.find(item => item.id === id);
+  }
+
+  const toggleComplete = (id) => {
+    const item = findComplete(id);
+    if (item.done){
+      const newItem = {
+        id: item.id,
+        text: item.text,
+        done: !item.done
+      };
+      const filteredComplete = complete.filter(item => item.id !== id);
+      setComplete(filteredComplete);
+      setTodo([...todo, newItem]);
+    }
+    else {
+      const item = findItem(id);
+      const newItem = {
+        id: item.id,
+        text: item.text,
+        done: true
+      }
+      const filteredtoDos = todo.filter(item => item.id !== id);
+      setComplete([...complete, newItem]);
+      setTodo(filteredtoDos);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoList>
+        {todo.map((item,index) => <TodoItem key={index} id={item.id} text={item.text} done={item.done} handleDone={setDone} toggleComplete={addComplete} /> )}
+        <br />
+        {complete.map((item,index) => <TodoItem key={index} id={item.id} text={item.text} done={item.done} handleDone={setDone} toggleComplete={toggleComplete} /> )}
+      </TodoList>
+      <TodoForm addItem={addItem} />
     </div>
   );
 }
---
+++ src/utils/js/randomString.js
@@ -0,0 +1,34 @@
+/**
+ * Generate a secure random string using the browser crypto functions
+ * @return {String} A random string
+ */
+ export default function generateRandomString () {
+	var array = new Uint32Array(28);
+	window.crypto.getRandomValues(array);
+	return Array.from(array, dec => ('0' + dec.toString(16)).substr(-2)).join('');
+}
+
+/**
+ * Calculate the SHA256 hash of the input text
+ * @param  {String}  plain The plaintext string
+ * @return {Promise}       Resolves as a hash
+ */
+function sha256 (plain) {
+	const encoder = new TextEncoder();
+	const data = encoder.encode(plain);
+	return window.crypto.subtle.digest('SHA-256', data);
+}
+
+/**
+ * Base64-urlencode a string
+ * @param  {String} str The unencoded string
+ * @return {String}     The encoded string
+ */
+function base64urlencode (str) {
+	// Convert the ArrayBuffer to string using Uint8 array to conver to what btoa accepts.
+	// btoa accepts chars only within ascii 0-255 and base64 encodes them.
+	// Then convert the base64 encoded to base64url encoded
+	// (replace + with -, replace / with _, trim trailing =)
+	return btoa(String.fromCharCode.apply(null, new Uint8Array(str)))
+		.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
